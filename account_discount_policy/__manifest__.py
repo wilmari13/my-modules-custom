99350c9ee47f93f5cf3ebf31e7863024ead8166b
wilmari13 <wilmarichacin@gmail.com>
2026-01-15 11:58:49 -0400

    [ADD] dependencias

--- account_discount_policy/__manifest__.py
+++ account_discount_policy/__manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account',],
+    'depends': ['base','account','contacts'],
 
     # always loaded
     'data': [
