0754f07c896ab77f684f56fb2207220f6e753b31
wilmari13 <wilmarichacin@gmail.com>
2026-01-15 11:56:21 -0400

    [ADD] prueba validar si se agrupa por etiqueta

--- smart_storage_tags/tests/test_storage_tags.py
+++ smart_storage_tags/tests/test_storage_tags.py
@@ -16,3 +16,7 @@ class TestStorageTags(TransactionCase):
         """Valida la asignación de etiquetas a productos"""
         self.product.storage_tag_ids = [(4, self.tag.id)]
         self.assertIn(self.tag, self.product.storage_tag_ids, "La etiqueta no se asignó correctamente.")
+
+        # Buscamos productos que tengan esta etiqueta
+        found = self.env['product.template'].search([('storage_tag_ids', 'in', self.tag.id)])
+        self.assertIn(self.product, found, "El producto no aparece bajo la etiqueta en el Kanban.")
