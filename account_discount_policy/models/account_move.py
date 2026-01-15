671a86c14490aa4e48c3a96d4794bec6c951d656
wilmari13 <wilmarichacin@gmail.com>
2026-01-15 08:44:34 -0400

    [FIX] onchange para cuando el parnert no posee descuento

--- account_discount_policy/models/account_move.py
+++ account_discount_policy/models/account_move.py
@@ -13,14 +13,21 @@ class ResPartner(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    @api.onchange('invoice_line_ids')
+    @api.onchange('invoice_line_ids', 'partner_id')
     def _onchange_discount_policy_id(self):
-        if self.partner_id.discount_policy_id:
+        # 1. Verificamos que haya un partner y que ese partner tenga política
+        if self.partner_id and self.partner_id.discount_policy_id:
+            policy_discount = self.partner_id.discount_policy_id.discount_percentage
+            
             for line in self.invoice_line_ids:
-                # Solo aplicamos el descuento si la línea tiene un producto
-                # y si el descuento es diferente para evitar bucles
-                if line.discount != self.partner_id.discount_policy_id.discount_percentage:
-                    line.discount = self.partner_id.discount_policy_id.discount_percentage
-
+                # 2. Solo aplicamos si el descuento es diferente para evitar bucles 
+                if line.display_type not in ('line_section', 'line_note'):
+                    if line.discount != policy_discount:
+                        line.discount = policy_discount
+        
+        # Si el partner NO tiene política, resetear a 0:
+        elif self.partner_id and not self.partner_id.discount_policy_id:
+            for line in self.invoice_line_ids:
+                line.discount = 0.0
 
                     
