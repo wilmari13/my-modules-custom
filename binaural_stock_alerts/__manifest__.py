28bac42d28fca92ca897904ac2f307bedc4328e8
wilmari13 <wilmarichacin@gmail.com>
2026-02-06 10:43:27 -0400

    [FIX] quitando carpeta controllers

--- binaural_stock_alerts/__manifest__.py
+++ binaural_stock_alerts/__manifest__.py
@@ -22,7 +22,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/product_template_views.xml',
     ],
     # only loaded in demonstration mode
